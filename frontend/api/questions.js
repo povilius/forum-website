07f64c59824358108ecede31589615e8691d3fe3
povilius <povilius@yahoo.com>
2024-01-05 18:50:16 +0200

    used axios

--- frontend/api/questions.js
+++ frontend/api/questions.js
@@ -1,27 +1,19 @@
 import { API } from "./consts"
 import axios from 'axios';
 
-const API_BASE_URL = `http://localhost:3000`
-
-const api = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
 
 export const fetchQuestions = async () => {
-  const response = await api.get('/questions');
+  const response = await axios.get(`${API}/questions`);
   return response.data;
 };
 
 export const createQuestion = async (question) => {
-  const response = await api.post('/questions', question);
+  const response = await axios.post(`${API}/posts`, question);
   return response.data;
 };
 
 export const deleteQuestion = async (id) => {
-  const response = await api.delete(`/questions/${id}`);
+  const response = await axios.delete(`${API}/questions/${id}`);
   return response.data;
 };
 
